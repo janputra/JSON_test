675a5e3331d941e80b3066dd72e05e517bc3d7a0
Jan Sembiring <janpoetra22@gmail.com>
2022-10-24 00:02:51 +0900

    update

--- main.cpp
+++ main.cpp
@@ -9,8 +9,39 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QString path = "/home/jan/JSON_test/test.json";
+
+    QList<QVariant> data;
+
+    QJsonObject _obj{
+      {"Test1","Value1"},
+      {"Test2","Value2"}
+    };
+
+
+    data.append(QStringList{"Test1","Test2","Test3"});
+
+    QJsonDocument inputDoc = QJsonDocument::fromVariant(QStringList{"Test1","Test2","Test3","Test4"});
+    inputDoc.setObject(_obj);
+    QString path = "/home/jan/Project/JSON_test/test2.json";
+    QByteArray bytes =  inputDoc.toJson(QJsonDocument::Indented);
+
     QFile file(path);
+
+    if(file.open(QIODevice::WriteOnly|QIODevice::Text|QIODevice::Truncate)){
+        QTextStream iStream(&file);
+        iStream.setCodec("utf-8");
+        iStream<<bytes;
+        file.close();
+
+    }
+    else{
+        std::cout<<"file open failed"<<std::endl;
+
+
+    }
+
+
+   // QFile file(path);
 
     if(file.open(QIODevice::ReadOnly))
     {
@@ -27,6 +58,7 @@ int main(int argc, char *argv[])
             std::cout<<"fromJson Success  "<<std::endl;
 
             QJsonObject obj = doc.object();
+
             QStringList keys = obj.keys();
             for (auto key: keys){
                 auto value = obj.take(key);
